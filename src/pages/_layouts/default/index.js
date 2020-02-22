4d192518b3112c0fa035b424b8028c096ffafd1d
Julio Martins de Sousa <jmasousa@voegol.com.br>
2020-02-22 17:07:16 -0300

    Utilizando Root Import

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 
 import { Wrapper } from './styles';
 
-// import { Container } from './styles';
-
 export default function DefaultLayout({ children }) {
 	return <Wrapper>{children}</Wrapper>;
 }
